5e6f496b2964fcc47e721e16ccd32c7f2ac92fe6
Roderik Schiele <roderik@esbl.digital>
2022-02-11 09:53:18 +0100

    luxery hotel

---
+++ index.vue
@@ -0,0 +1,67 @@
+<div class="row menuBar">
+            <div class="col-xl-2 col-lg-2 col-6 align-self-center">
+                <img src="images/lhc_logo.svg" alt="Luxury Hotel Company" id="logo">
+            </div>
+            <div class="col-xl-2 col-lg-2 col-6 align-self-center">
+                <div class="trustpilot" id="trustpilot">
+                    <div class="trustpilot-widget" data-locale="nl-NL" data-template-id="53aa8807dec7e10d38f59f32" data-businessunit-id="51968b1800006400053240c1" data-style-height="90px" data-style-width="120px" data-theme="light">
+                        <a href="https://nl.trustpilot.com/review/luxuryhotelcompany.com" target="_blank" rel="noopener">Trustpilot</a>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-xl-2 col-lg-2 col-6 offset-lg-3 offset-0 align-self-center">
+               
+                <img src="images/nl.png" class="flag" alt="flag" id="flag">
+                <span id="language-name">
+                    Nederlands
+                </span>            
+            </div>
+           
+            <div class="col-xl-1 col-lg-1 align-self-center" id="linkedIn">
+                <div class="align-self-center lindsey">
+                    <img src="images/linsday_foto.png" class="align-self-center" alt="Foto Lindsey" id="profilePic">                
+                    <div class="wrapper">
+                        <div>
+                            <img src="images/linkedin_logo.png" alt="linkedIn" id="linkedIn-logo"><span class="details">Lindsey van der Weele</span>
+                        </div>
+
+                        <div>
+                            <img src="images/phone-icon.png" alt="Phone Icon" id="phone-icon"><span class="details">+31 6 25 223 054</span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="header" style="width: 100%;">
+            <img src="images/header_gradient.png" style="height: 581px; width: 100%; object-fit:cover;">
+            <div class="header-bigText">
+            #1 overcapaciteit oplossing voor 4 en 5 sterrenhotels
+            </div>
+            <div class="header-smallText">
+                Gemiddeld genomen blijft 40% van de hotelcapaciteit onbenut. 
+                Een gemiste kans omdat u als eigenaar niet alleen de kameropbrengst, maar ook aditionele inkomsten misloopt. 
+                Bereken hier eenvoudig wat u kunt verdienen met onze oplossing.
+            </div>
+
+            <div class="revenue-calculator">
+                <div class="revenue-calculator-title">
+                    Extra omzet berekenen
+                </div>
+                <form action="">
+
+                <div class="total-rooms-text">
+                    Totaal aantal kamers
+                </div>
+
+                
+                    <input class="input-total-rooms" type="text" name="total_rooms" placeholder="0"></input>
+                
+
+                </form>
+
+
+            </div>
+        </div>
+        
